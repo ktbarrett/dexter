d5549c72b4355683f0f2f0387b40e2cc7507748e
Kaleb Barrett <dev.ktbarrett@gmail.com>
2026-02-15 15:58:47 -0700

    Added Task object model and dec for parsing info from func sigs

--- src/dexter/__init__.py
+++ src/dexter/__init__.py
@@ -1,1 +1,12 @@
 from __future__ import annotations
+
+from dexter._decorator import task
+from dexter._task import Task
+
+__all__ = [
+    "Task",
+    "task",
+]
+
+for name in __all__:
+    globals()[name].__module__ = __name__
