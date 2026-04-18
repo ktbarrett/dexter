0f76df9b359e4f75f398d0396ce97fc16c24c918
Kaleb Barrett <dev.ktbarrett@gmail.com>
2026-04-17 20:58:04 -0600

    Fix mypy error on Task.__name__ field

--- src/dexter/_task.py
+++ src/dexter/_task.py
@@ -26,7 +26,7 @@ class Empty: ...
 empty = Empty()
 
 
-@dataclass(kw_only=True, unsafe_hash=True)
+@dataclass(kw_only=True)
 class Arg(Generic[T]):
     name: str
     default: T | Empty = empty
@@ -36,7 +36,10 @@ class Arg(Generic[T]):
     choices: Sequence[T] | None = None
 
 
-@dataclass(kw_only=True, unsafe_hash=True)
+# Making this a dataclass causes issues with mypy. If we don't define the __name__,
+# __qualname__, __doc__, and __module__ attribute annotations, then we can't use those
+# attributes on Task objects without mypy complaining. But if we define them, they have
+# to be defaulted dataclass.fields, which is weird and slow and unnecessary.
 class Task(Generic[Params, Result]):
     name: str
     description: str
@@ -50,7 +53,21 @@ class Task(Generic[Params, Result]):
     __doc__: str | None
     __module__: str
 
-    def __post_init__(self) -> None:
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        args: Sequence[Arg[Any]],
+        predecessors: Sequence[Task[Any, Any] | str],
+        successors: Sequence[Task[Any, Any] | str],
+        body: Callable[Params, Result],
+    ) -> None:
+        self.name = name
+        self.description = description
+        self.args = args
+        self.predecessors = predecessors
+        self.successors = successors
+        self.body = body
         update_wrapper(self, self.body)
 
     def __call__(self, *args: Params.args, **kwargs: Params.kwargs) -> Result:
